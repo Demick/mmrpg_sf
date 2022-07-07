5cb8513b17af5e4417e84eb1e6b964c768db2e4c
Erdem Norboev <erdem1600@gmail.com>
2022-07-07 19:35:36 +0300

    first commit

---
+++ fun_web/fun_web/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fun_web.users.models import User
+from fun_web.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
